b3b1ff2949e4bbaf0d09b3a29b6505096e5ef9e1
parametprame <parametprame2@gmail.com>
2021-01-27 01:29:40 +0700

    uppp

--- src/pages/Diagnosis.js
+++ src/pages/Diagnosis.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
